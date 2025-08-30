45e33afa312c4dc68b2851e6e501737074502270
Stefano Gini <61376940+Haruno19@users.noreply.github.com>
2025-08-30 14:54:03 +0200

    Delete Windows/Current directory

--- Windows/Current/Microsoft.PowerShell_profile.ps1
+++
@@ -1,6 +0,0 @@
-Invoke-Expression (& { (zoxide init powershell | Out-String) })
-
-Set-Alias -Name "cd" -Value z -Option AllScope
-
-Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
-Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
