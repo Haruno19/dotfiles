e3bce2091839b9310195da138df3e8932ecbaf91
Stefano Gini <61376940+Haruno19@users.noreply.github.com>
2025-08-31 11:28:12 +0200

    Update Microsoft.PowerShell_profile.ps1

--- Windows/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Windows/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -5,6 +5,23 @@ Set-Alias -Name "cd" -Value z -Option AllScope
 Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
 Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
 
+Register-ArgumentCompleter -CommandName cd, Set-Location -ParameterName Path -ScriptBlock {
+    param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameters)
+
+    [System.Management.Automation.CompletionCompleters]::CompleteFilename($wordToComplete) |
+        ForEach-Object {
+            $text = $_.CompletionText -replace '^(?:\./|\.\\)', ''
+            $listText = $_.ListItemText -replace '^(?:\./|\.\\)', ''
+
+            [System.Management.Automation.CompletionResult]::new(
+                $text,
+                $listText,
+                $_.ResultType,
+                $_.ToolTip
+            )
+        }
+}
+
 function prompt {
     $isAdmin = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")
     
@@ -37,4 +54,8 @@ function prompt {
     $dirPrompt = "$([char]27)[33m  $currentDir$([char]27)[0m"
     
     return $arrowPrompt + $dirPrompt + " "
+}
+
+function Refresh-Path {
+    $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
 }
